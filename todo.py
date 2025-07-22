a671bff81c7d3cd1ecdaa9d61351e4a0d5b14fc4
AaronEki <aaron.connolly61@gmail.com>
2025-07-22 15:14:04 +0100

    drafting outline for add_item function

--- todo.py
+++ todo.py
@@ -1,8 +1,40 @@
 # y = yank, p = paste, u = undo
 import json
 
-def add_item(file_path):
-    print("")
+# Add item function to add a new item to the to do list
+# ================
+# WORK IN PROGRESS
+# ================
+def add_item(file_path, name, subtask_list, due):
+    # read file and use the .append function to add the data to the file
+    with open(file_path, 'r+') as file:
+        file_data = json.load(file)
+
+        # Getting the ID of the last object in the file (+1)
+        new_id=file_data[-1]["id"] + 1
+
+        # Building the new json object
+        new_date = {
+            "id": new_id,
+            "task_name": name,
+            "subtasks": subtask_list,
+            "due": due,
+            "complete": False        
+        }
+
+        # Write the new json data to the database file
+        try:
+            file_data.append(new_data)
+            
+            # Move the cursor to the beginning of the file
+            file.seek(0)
+            
+            # Write the updated data back to the file
+            json.dump(file_data, file, indent=4)
+            print(f"Item ID: {new_id} successfully written to a file")
+
+        except Exception as e:
+            print(f"Exception raised: {e}")
 
 def del_item(file_path):
     # Add a "are you sure you want to delete {print item here} y/n before deletion]
@@ -54,6 +86,12 @@ def display_list(file_path):
 def main(file_path):
     # Add functionality for running the program with flags, calling different 
     display_list(file_path)
+
+    # Test the new add_item function
+    # name = <USER INPUT NAME flag? or stdin?>
+    # subtask_list = <build a user prompt with while loop to build up subtask list>
+    # due = <user input- flag or stdin?>
+    # add_item(file_path, name, subtask_list, due)
    
 if __name__ == "__main__":
     file_path = "/home/binieki/.local/util_files/database.json"
